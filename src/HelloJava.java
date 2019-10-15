3ea04680fac8a757bd5849ad90d4ae595ae93118
khedekarashwini <55612741+khedekarashwini@users.noreply.github.com>
2019-10-15 11:52:06 +0530

    Update HelloJava.java

--- src/HelloJava.java
+++ src/HelloJava.java
@@ -4,9 +4,9 @@ public class HelloJava {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello world");
-		System.out.println("New git Prog");
-//
-		//
+		System.out.println("New git Prog")
+	   		System.out.println("Hiii")
+		
 	}
 
 }
